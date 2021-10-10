ca72902b6e720cd3c0c894c17f297ce947bf0f89
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2021-10-10 14:13:27 +0200

    Update 1-simple.php

--- Images/Width-height/1-simple.php
+++ Images/Width-height/1-simple.php
@@ -1,6 +1,7 @@
 <?php
-$image_path ="https://test.scriptphp.eu/img/php.png";
+$image_path ="../../img/php.png";
+// ou
+//$image_path ="https://test.scriptphp.eu/img/php.png";
 list($width, $height, $type, $attr)= getimagesize($image_path) ;
 echo "width = ".$width." height = ". $height."<br>";
-echo "Type = ".$type." Extension = ". $attr."<br>";
 ?>
