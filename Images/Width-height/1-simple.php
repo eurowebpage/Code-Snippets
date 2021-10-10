fc65eac5b0ea9c221f3dbe5a09f364856f68286c
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2021-10-10 14:38:15 +0200

    Update 1-simple.php

--- Images/Width-height/1-simple.php
+++ Images/Width-height/1-simple.php
@@ -1,7 +1,7 @@
 <?php
 $image_path ="../../img/php.png";
-// ou
-//$image_path ="https://test.scriptphp.eu/img/php.png";
-list($width, $height, $type, $attr)= getimagesize($image_path) ;
+$image_url ="https://test.scriptphp.eu/img/php.png";
+list($width, $height, $type, $attr)=  getimagesize($image_path) ;
 echo "width = ".$width." height = ". $height."<br>";
+echo '<img src= "'.$image_url.'"><hr>';
 ?>
