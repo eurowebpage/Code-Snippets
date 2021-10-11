a86a98be9c54cf70fee131be0bdbd8692ed830d2
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2021-10-11 16:25:58 +0200

    Update jours-date-lang.php

--- Date-Hour/Date/jours-date-lang.php
+++ Date-Hour/Date/jours-date-lang.php
@@ -12,3 +12,20 @@ $mois_nom = date("M");
 $annee = date("Y");
 $date_simple = date("Y-m-d");
 ?>
+<!-- Html -->
+<h2 class="mb-3">2. Lang - Pays</h2>
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+<p>A. SYSTEM : <?php echo $sys_date; ?><br> <?php  echo strftime("%A"); ?></p>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+<p>B. USA : <?php echo $usa_date; ?><br> <?php  $locale = 'en_US.utf8'; setlocale(LC_ALL, $locale); echo strftime('%A',strtotime($jour_lang));
+ ?></p>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+<p>C. Bruxelles : <?php echo $bxl_date; ?><br> <?php  $locale = 'fr_FR.utf8'; setlocale(LC_ALL, $locale); echo strftime('%A',strtotime($jour_lang)); ?></p>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+<p>D. Athens : <?php echo $gr_date; ?><br> <?php   $locale2 = 'el_GR.UTF-8'; setlocale(LC_ALL, $locale2); echo strftime('%A',strtotime($jour_lang)); ?></p>
+</div>
+</div>
