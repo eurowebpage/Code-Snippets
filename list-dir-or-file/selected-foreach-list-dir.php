6cd70a8fdca7f8786123c9cd2bebc4f8931fdebf
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2021-12-16 13:02:30 +0100

    Create selected-foreach-list-dir.php

---
+++ list-dir-or-file/selected-foreach-list-dir.php
@@ -0,0 +1,23 @@
+<div class="form-group">
+<label for="sel1"><h2>Push E-mail address</h2></label>
+<select class="form-control" id="sampleSelect">
+      <?php
+	  
+$dirs = array_filter(glob('*'), 'is_dir');
+foreach($dirs as $dir){
+	echo "<option  value='".$dir."/index.php'>".$dir."</option>";
+	}
+
+	  ?>
+</select> 
+<script>
+$("select").click(function() {
+  var open = $(this).data("isopen");
+  if(open) {
+    window.location.href = $(this).val()
+  }
+  
+  $(this).data("isopen", !open);
+});
+</script>           
+        </div>
