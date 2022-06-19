289a069c1a309bfdd56f2f8793abb12b5e5f5bca
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2022-06-19 08:57:11 +0200

    Create contact.php

---
+++ Form/Mail-Contact/1/contact.php
@@ -0,0 +1,22 @@
+				<h3>Formulaire Contact</h3>
+									<form method="post" action="send_form_email.php">
+										<div class="row uniform">
+											<div class="6u 12u$(xsmall)">
+                                        <input type="text" class="text" id="first_name" name="first_name" placeholder="Nom"/>											</div>
+                                            <div class="6u 12u$(xsmall)">
+												<input class="text" type="text"  id="last_name" name="last_name" placeholder="Prenom"/>
+											</div>
+											<div class="6u$ 12u$(xsmall)">
+												<input type="text" class="text" id="email" name="email" placeholder="Email "/>
+											</div>
+											<div class="12u$">
+												<textarea id="comments" name="comments" placeholder="Message" rows="6"></textarea>
+											</div>
+											<div class="12u$">
+												<ul class="actions">
+													<li><input type="submit" value="Envoyer" /></li>
+													<li><input type="reset" value="Effacer" /></li>
+												</ul>
+											</div>
+										</div>
+									</form>
