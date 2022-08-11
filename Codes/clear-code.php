f389b30e1bbdd4f1f976bf8838c1697dd4d40858
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2022-08-11 09:45:55 +0200

    Update clear-code.php

--- Codes/clear-code.php
+++ Codes/clear-code.php
@@ -1,1 +1,10 @@
 
+<?php
+$file_source = "cookies.php";
+$file_clear = "cookies.php";
+$str = file_get_contents($file_source);
+$str = preg_replace('/\n(\s*\n)+/', "\n", $str); // supprime toutes les lignes blanche
+$str = preg_replace("/<!--((?!batcache)(?!\\[endif\\])[\\s\\S])*?-->/", "", $str); // supprime les comentaires html
+$str = preg_replace("/^\s+/m", " ", $str); // supprime espaces blanc debut de ligne
+file_put_contents($file_clear, $str);
+?>
