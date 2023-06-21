732ee19d6e021abdfd2abd9f596403e6821806d5
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2023-06-21 17:46:07 +0200

    Create clear-code-get-request.php
    clear code with REQUEST METHOD GET

---
+++ Codes/GET/clear-code-get-request.php
@@ -0,0 +1,17 @@
+<?php
+############# Clean Code #############
+if (isset($_GET["cc"]) ? $_GET["cc"] : null) {
+$cc_action = $_GET["cc"];
+if($cc_action == "ok"){
+$actual_filename = basename($_SERVER['PHP_SELF']);
+$file_source = $actual_filename;
+$file_clear = $actual_filename;
+$str = file_get_contents($file_source);
+$str = preg_replace('/\n(\s*\n)+/', "\n", $str); // supprime toutes les lignes blanche
+$str = preg_replace("/<!--((?!batcache)(?!\\[endif\\])[\\s\\S])*?-->/", "", $str); // supprime les comentaires html
+$str = preg_replace("/^\s+/m", " ", $str); // supprime espaces blanc debut de ligne
+file_put_contents($file_clear, $str);
+}
+}
+############# Fin Clean Code #############
+?>
