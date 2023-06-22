a2fde7015c78292e4dd4de26c28dd8117429a0cc
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2023-06-22 23:11:53 +0200

    Update dirlist.php

--- DIR/Function/dirlist.php
+++ DIR/Function/dirlist.php
@@ -1,20 +1,20 @@
 <?php
-function dirlist($loc,$class,$start_tag,$end_tag,$other){
+function dirlist($loc,$link,$class,$start_tag,$end_tag,$other){
 $scandir = scandir($loc);
 $scandir = array_filter($scandir, function($element){
 return !preg_match('/^\./', $element);
 });
 if(empty($scandir)) echo '<p style="color:red">Empty Dir</p>';
 $listdirectories = "";
-foreach($scandir as $file){
-$baseLink = $loc . DIRECTORY_SEPARATOR . $file;
+foreach($scandir as $dir){
+$baseLink = $loc . DIRECTORY_SEPARATOR . $dir;
 if(is_dir($baseLink)){
-$listdirectories .= ''.$start_tag.'<a class="'.$class.'" href="'.$file.'" '.$other.' title="'.$file.'">'.$file.'</a>'.$end_tag.'';
+$listdirectories .= ''.$start_tag.'<a class="'.$class.'" href="'.$link.$dir.'" '.$other.' title="'.$dir.'">'.$dir.'</a>'.$end_tag.'';
 }
 }
 return $listdirectories;
 }
+// example : dirlist($path_server,$pluginurl,"dropdown-item2","<p>","</p>","target='_blank'");
 ?>
 <!-- Exemple  -->
-<?php echo dirlist($path_server,"dropdown-item2","<p>","</p>","target='_blank'");?>
-
+<?php echo dirlist($path_server."setting/front/",$pluginurl."setting/front/","dropdown-item","","","");?>
