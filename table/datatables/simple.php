8bd14a8e553776b4b3ea00a83c186a59c4410e97
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2023-06-23 22:07:10 +0200

    Create simple.php

---
+++ table/datatables/simple.php
@@ -0,0 +1,16 @@
+<link href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+<script>
+$(document).ready(function() {
+$('#example').DataTable();
+} );
+</script>
+<style>
+table.dataTable{border-collapse:collapse !important;}
+
+a.disabled {
+  color: gray;
+  pointer-events: none;
+}    
+</style>
