722b1139d033f75f7cd0d6cba9fc20954acca417
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2024-03-22 06:14:02 +0100

    Create 1.php

---
+++ html/Template/start-simple-page/1.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Script PHP - SEO Remote share test 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Description original">
+<meta property="og:title" content="Title original">
+<meta property="og:image" content="https://your-domain.eu">
+<meta property="og:site_name" content="your-domain.eu">
+<meta property="og:description" content="Description original">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid mt-3">
+  <h2>Start page</h2>
+
+</div>
+
+</body>
+</html>
